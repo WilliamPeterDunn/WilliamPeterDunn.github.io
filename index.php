5bfca32acca236928b91213795d8560252f1ce37
William Dunn <williampeterdunn@gmail.com>
2023-01-24 22:28:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html class='h-100'>
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>William Dunn</title>
+	  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+	  <style>
+	  body{
+		  background-color: darkgrey;
+		  background-image: url('BG.jpg'); 
+		  
+	  }
+	  
+	  
+	  </style>
+
+	</head>
+	<body class='h-100'>
+	  <div class="container-fluid h-100">
+		<div class="row align-items-center justify-content-center">
+		  <div class="col-md-8">
+			<div class="card my-0 h-100 bg-dark text-light" style="--bs-bg-opacity: .8;">
+			  <div class="card-body">
+				<header>
+				  <h1>William Dunn</h1>
+				  <p>Microsoft Data Platform Engineer | SQL &amp BI Developer </p>
+				</header>
+				 <section>
+				  <h2>About me</h2>
+				  <p>Hello! I'm a MS SQL and BI developer specialising in the Microsoft data stack. I am based in Durbanville, near Cape Town, in South Africa.</p>
+				  <p>SQL Server, Reporting Services, and Power BI are what I do best, but I am interested in all kinds of tech. My current interests include learning more about the Azure platform and web development.</p>
+				</section>
+				<section>
+				  <h2>Resume</h2>
+				  <p>2019 - 2023 Freelance Data Engineer</p>
+				  <p>2017 - 2019 Report / BI Developer</p>
+				  <p>2012 - 2019 Tech Support - SQL Server, SSRS, Power BI, MS Access, and Excel</p>
+				  <p>2009 - 2012 SOP - Access, Excel</p>
+				  <p>B.Sc Hons (Computing) | UNISA | 2020</p>
+				  <p>B.Com (Informatics) | UNISA | 2015</p>
+				  <p>Microsoft Certified: Power BI Data Analyst</p>
+				  <p>Microsoft Certified: Azure Data Scientist</p>
+				</section>
+				
+				<section class='row'>
+				  <form class='offset-lg-3 col-lg-6 border p-2 rounded text-center'>
+					<div class="form-group">
+					  <label for="name">Name</label>
+					  <input type="text" class="form-control" id="name" placeholder="Your name">
+					</div>
+					<div class="form-group">
+					  <label for="message">Message</label>
+					  <textarea class="form-control" id="message" rows="3"></textarea>
+					</div>
+					<button type="submit" class=" m-1 btn btn-primary">Send</button>
+				  </form>
+				</section>
+				
+				<footer class='text-center'>
+				  <a href="https://www.linkedin.com/in/williampeterdunn/">LinkedIn</a> | <a href="https://github.com/WilliamPeterDunn">GitHub</a>
+				</footer>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</body>
+</html>
